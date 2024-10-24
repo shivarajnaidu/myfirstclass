07ad122d23e6b47255a6a02e9388bd2d34ada7df
Yuvaraj V <yuvaraj.vemula@traceone.com>
2024-10-24 20:04:39 +0530

    animation complted

---
+++ testscript.js
@@ -0,0 +1,18 @@
+function handleClick() {
+    //this is alert message
+    /* this is multi line comment 
+    msn d
+    sdlknfl
+    */
+    // window.alert('i am external')
+  }
+
+
+  /*
+  data types
+  variables
+  conditions
+  loops
+  functions
+  error handling
+  */
